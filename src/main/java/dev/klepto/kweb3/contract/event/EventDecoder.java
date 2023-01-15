7cfa5b1c1c63f1eed32028a1682f714094bdb995
klepto <klepto@inbox.lt>
2023-01-15 17:46:00 +0200

    Cache event definitions, fix event decoder for matching signatures with different amount of indexed values

--- src/main/java/dev/klepto/kweb3/contract/event/EventDecoder.java
+++ src/main/java/dev/klepto/kweb3/contract/event/EventDecoder.java
@@ -6,9 +6,12 @@ import dev.klepto.kweb3.contract.*;
 import dev.klepto.kweb3.util.Keccak;
 import io.github.classgraph.ClassGraph;
 import lombok.SneakyThrows;
+import lombok.Value;
 import lombok.val;
 
+import java.lang.reflect.Field;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
@@ -16,18 +19,14 @@ import java.util.*;
  */
 public class EventDecoder {
 
-    private final List<Class<?>> eventClasses;
-    private final Map<Class<?>, ValueType> eventTypes;
-    private final Map<Class<?>, String> eventSignatures;
+    private final Map<Class<?>, EventDefinition> definitions;
 
     public EventDecoder() {
         this(findEventClasses());
     }
 
     public EventDecoder(List<Class<?>> eventClasses) {
-        this.eventClasses = eventClasses;
-        this.eventTypes = getEventTypes();
-        this.eventSignatures = getEventSignatures();
+        this.definitions = getEventDefinitions(eventClasses);
     }
 
     public Events decode(Web3Response response) {
@@ -43,17 +42,21 @@ public class EventDecoder {
 
             var logEvent = (Object) null;
             val logSignature = log.getTopics().get(0);
-            for (val eventType : eventClasses) {
-                val eventSignature = eventSignatures.get(eventType);
+            for (val eventDefinition : definitions.values()) {
+                val eventSignature = eventDefinition.getSignature();
                 if (!eventSignature.equals(logSignature)) {
                     continue;
                 }
 
+                if (log.getTopics().size() - 1 != eventDefinition.getIndexedFields().size()) {
+                    continue;
+                }
+
                 if (logEvent == null) {
-                    logEvent = decodeEvent(client, eventType, log);
+                    logEvent = decodeEvent(client, eventDefinition, log);
                     result.add(logEvent);
                 } else {
-                    result.add(copyEvent(logEvent, eventType));
+                    result.add(copyEvent(logEvent, eventDefinition));
                 }
             }
         }
@@ -61,27 +64,22 @@ public class EventDecoder {
     }
 
     @SneakyThrows
-    private Object decodeEvent(Web3Client client, Class<?> eventClass, Web3Response.Log log) {
-        val eventFields = Arrays.asList(eventClass.getDeclaredFields());
-        val eventType = eventTypes.get(eventClass);
-        val valueFields = eventFields.stream()
-                .filter(field -> !field.isAnnotationPresent(Event.Indexed.class))
-                .filter(field -> !field.isAnnotationPresent(Event.Address.class))
-                .toList();
-        val valueType = ContractCodec.parseFieldsType(valueFields);
+    private Object decodeEvent(Web3Client client, EventDefinition definition, Web3Response.Log log) {
+        val fields = definition.getFields();
+        val indexedType = definition.getIndexedType();
+        val valueType = definition.getValueType();
         val value = client.abiDecode(log.getData(), valueType.getAbiType());
 
         var valueOffset = 0;
         var topicOffset = 1;
         val parameters = new ArrayList<>();
-        for (var i = 0; i < eventFields.size(); i++) {
-            val field = eventFields.get(i);
+        for (val field : fields) {
             var fieldValue = (Object) null;
 
             if (field.isAnnotationPresent(Event.Address.class)) {
                 fieldValue = log.getAddress();
             } else if (field.isAnnotationPresent(Event.Indexed.class)) {
-                val fieldType = eventType.getChildren().get(i);
+                val fieldType = indexedType.getChildren().get(topicOffset - 1);
                 val fieldAbi = log.getTopics().get(topicOffset++);
                 fieldValue = client.abiDecode(fieldAbi, fieldType.getAbiType()).get(0);
             } else {
@@ -91,41 +89,55 @@ public class EventDecoder {
             parameters.add(fieldValue);
         }
 
+        val eventClass = definition.getEventClass();
         val constructor = eventClass.getDeclaredConstructors()[0];
         constructor.setAccessible(true);
         return constructor.newInstance(parameters.toArray());
     }
 
-    private Map<Class<?>, ValueType> getEventTypes() {
-        val result = new HashMap<Class<?>, ValueType>();
-        for (val type : eventClasses) {
-            val fields = Arrays.asList(type.getDeclaredFields());
-            result.put(type, ContractCodec.parseFieldsType(fields));
-        }
-        return result;
+
+    private static Map<Class<?>, EventDefinition> getEventDefinitions(List<Class<?>> eventClasses) {
+        return eventClasses.stream().collect(Collectors.toMap(key -> key, key -> getEventDefinition(key)));
     }
 
-    private Map<Class<?>, String> getEventSignatures() {
-        val result = new HashMap<Class<?>, String>();
-        for (val type : eventClasses) {
-            var eventName = type.getAnnotation(Event.class).value();
-            if (eventName.isBlank()) {
-                eventName = type.getSimpleName();
-            }
+    private static EventDefinition getEventDefinition(Class<?> eventClass) {
+        val fields = Arrays.stream(eventClass.getDeclaredFields()).toList();
+        val nonAddressFields = fields.stream()
+                .filter(field -> !field.isAnnotationPresent(Event.Address.class))
+                .toList();
+        val valueFields = fields.stream()
+                .filter(field -> !field.isAnnotationPresent(Event.Indexed.class))
+                .filter(field -> !field.isAnnotationPresent(Event.Address.class))
+                .toList();
+        val indexedFields = fields.stream()
+                .filter(field -> field.isAnnotationPresent(Event.Indexed.class))
+                .toList();
+        val indexedType = ContractCodec.parseFieldsType(indexedFields);
+        val valueType = ContractCodec.parseFieldsType(valueFields);
 
-            val eventFields = Arrays.stream(type.getDeclaredFields())
-                    .filter(field -> !field.isAnnotationPresent(Event.Address.class))
-                    .toList();
-            val eventType = ContractCodec.parseFieldsType(eventFields);
-            val eventSignature = eventName + eventType.getAbiType().toString();
-            val eventHash = "0x" + Keccak.hash(eventSignature);
-            result.put(type, eventHash);
+        var name = eventClass.getAnnotation(Event.class).value();
+        if (name.isBlank()) {
+            name = eventClass.getSimpleName();
         }
-        return result;
+
+        val signatureType = ContractCodec.parseFieldsType(nonAddressFields);
+        val signatureRaw = name + signatureType.getAbiType().toString();
+        val signature = "0x" + Keccak.hash(signatureRaw);
+        return new EventDefinition(
+                eventClass,
+                name,
+                signature,
+                fields,
+                valueFields,
+                indexedFields,
+                indexedType,
+                valueType
+        );
     }
 
     @SneakyThrows
-    private static Object copyEvent(Object event, Class<?> eventClass) {
+    private static Object copyEvent(Object event, EventDefinition definition) {
+        val eventClass = definition.getEventClass();
         val fields = event.getClass().getDeclaredFields();
         val parameters = new ArrayList<>();
         for (var i = 0; i < fields.length; i++) {
@@ -147,6 +159,18 @@ public class EventDecoder {
             }
         }
         return eventClasses;
+    }
+
+    @Value
+    private static class EventDefinition {
+        Class<?> eventClass;
+        String name;
+        String signature;
+        List<Field> fields;
+        List<Field> valueFields;
+        List<Field> indexedFields;
+        ValueType indexedType;
+        ValueType valueType;
     }
 
 }
