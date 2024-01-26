2e2b6a0a39362a250c991bb055e646305ed057ca
klepto <klepto@inbox.lt>
2024-01-26 14:38:56 +0200

    fix: Add eth_sendRawTransaction implementation to RpcApi

--- src/main/java/dev/klepto/kweb3/rpc/RpcApi.java
+++ src/main/java/dev/klepto/kweb3/rpc/RpcApi.java
@@ -3,13 +3,14 @@ package dev.klepto.kweb3.rpc;
 import dev.klepto.kweb3.rpc.api.EthCall;
 import dev.klepto.kweb3.rpc.api.EthEstimateGas;
 import dev.klepto.kweb3.rpc.api.EthGasPrice;
+import dev.klepto.kweb3.rpc.api.EthSendRawTransaction;
 
 /**
  * Represents Ethereum RPC API implementing all the supported methods.
  *
  * @author <a href="http://github.com/klepto">Augustinas R.</a>
  */
-public interface RpcApi extends EthCall, EthGasPrice, EthEstimateGas {
+public interface RpcApi extends EthCall, EthGasPrice, EthEstimateGas, EthSendRawTransaction {
 
     /**
      * Default JSON RPC version.
