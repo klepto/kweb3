f358adf1d8c57d2d6a5e924d36b9842fc7469840
klepto <klepto@inbox.lt>
2024-01-31 21:51:49 +0200

    feat: Implement useful stacktrace clean-up methods for Web3Error

--- src/main/java/dev/klepto/kweb3/Web3Error.java
+++ src/main/java/dev/klepto/kweb3/Web3Error.java
@@ -1,9 +1,13 @@
 package dev.klepto.kweb3;
 
+import lombok.val;
 import org.slf4j.helpers.MessageFormatter;
 
+import java.util.Arrays;
+
 /**
- * Represents kweb3 runtime error.
+ * Represents kweb3 runtime error. Contains various helper methods for stacktrace manipulation in-order to produce
+ * cleaner error messages in-cases such as using {@link java.lang.reflect.Proxy} calls.
  *
  * @author <a href="http://github.com/klepto">Augustinas R.</a>
  */
@@ -19,6 +23,47 @@ public class Web3Error extends RuntimeException {
 
     public Web3Error(String message, Object... args) {
         super(MessageFormatter.arrayFormat(message, args).getMessage());
+    }
+
+    /**
+     * Replaces stack trace class name at a given depth. Used for cleaner exception
+     *
+     * @param depth the target depth
+     * @return this exception instance
+     */
+    public Web3Error replaceStackClass(int depth, Class<?> newClass) {
+        val stackTrace = getStackTrace();
+        if (depth >= stackTrace.length) {
+            return this;
+        }
+
+        val source = stackTrace[depth];
+        val stackTraceElement = new StackTraceElement(
+                newClass.toString(),
+                source.getMethodName(),
+                source.getFileName(),
+                source.getLineNumber()
+        );
+
+        stackTrace[depth] = stackTraceElement;
+        setStackTrace(stackTrace);
+        return this;
+    }
+
+    /**
+     * Discards stack trace up to a given depth. Used for cleaning up redundant exception stack traces.
+     *
+     * @param depth the target depth
+     * @return this exception instance
+     */
+    public Web3Error discardStack(int depth) {
+        val stackTrace = getStackTrace();
+        if (depth >= stackTrace.length) {
+            return this;
+        }
+
+        setStackTrace(Arrays.copyOfRange(stackTrace, depth, stackTrace.length));
+        return this;
     }
 
 }
