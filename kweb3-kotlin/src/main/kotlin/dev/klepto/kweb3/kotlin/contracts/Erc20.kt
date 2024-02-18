d517dbf54f005d246c53f47654a538db1bd24d36
klepto <klepto@inbox.lt>
2024-02-18 20:36:32 +0200

    feat: Implement suspending versions of Erc20 and Multicall3

---
+++ kweb3-kotlin/src/main/kotlin/dev/klepto/kweb3/kotlin/contracts/Erc20.kt
@@ -0,0 +1,80 @@
+package dev.klepto.kweb3.kotlin.contracts
+
+import dev.klepto.kweb3.core.contract.Web3Contract
+import dev.klepto.kweb3.core.contract.annotation.ValueSize
+import dev.klepto.kweb3.core.contract.annotation.View
+import dev.klepto.kweb3.core.type.EthAddress
+import dev.klepto.kweb3.core.type.EthString
+import dev.klepto.kweb3.core.type.EthUint
+
+/**
+ * Suspending implementation of ERC-20 standard and commonly available
+ * functions of ERC-20 tokens.
+ *
+ * @author <a href="http://github.com/klepto">Augustinas R.</a>
+ */
+interface Erc20 : Web3Contract {
+
+    /**
+     * Returns the name of the token.
+     *
+     * @return the name of this token
+     */
+    @View
+    suspend fun name(): EthString
+
+    /**
+     * Returns the symbol of the token, usually a shorter version of the
+     * [Erc20.name].
+     *
+     * @return the symbol of this token
+     */
+    @View
+    suspend fun symbol(): EthString
+
+
+    /**
+     * Returns the value of tokens in existence represented as `uint256`.
+     *
+     * @return the value of tokens in existence
+     */
+    @View
+    suspend fun totalSupply(): EthUint
+
+    /**
+     * Returns the number of decimals used for token amount user
+     * representation. Most tokens use value of `18`, imitating the
+     * relationship between Ether and Wei. This information is only used
+     * for display purposes and does not influence arithmetic of the token
+     * contract.
+     *
+     * @return the number of decimals used to represent token values
+     */
+    @View
+    @ValueSize(8)
+    suspend fun decimals(): EthUint
+
+    /**
+     * Returns the value of tokens owned by a given account
+     * [address][EthAddress].
+     *
+     * @param account the address of the account
+     * @return the value of tokens owned by given account address
+     */
+    @View
+    suspend fun balanceOf(account: EthAddress): EthUint
+
+    /**
+     * Returns the remaining amount of tokens that `spender` will be allowed to
+     * spend on behalf of `owner` through `transferFrom` function. This value
+     * is zero by default.
+     *
+     * @param owner the address of the owner account
+     * @param spender the address of the spender
+     * @return the remaining amount of tokens that spender is allowed to spend
+     *     on behalf of owner
+     */
+    @View
+    suspend fun allowance(owner: EthAddress, spender: EthAddress): EthUint
+    
+}
---
+++ kweb3-kotlin/src/main/kotlin/dev/klepto/kweb3/kotlin/contracts/Multicall3.kt
@@ -0,0 +1,49 @@
+package dev.klepto.kweb3.kotlin.contracts
+
+import dev.klepto.kweb3.core.contract.Web3Contract
+import dev.klepto.kweb3.core.contract.annotation.View
+import dev.klepto.kweb3.core.contract.type.EthStructContainer
+import dev.klepto.kweb3.core.type.EthAddress
+import dev.klepto.kweb3.core.type.EthArray
+import dev.klepto.kweb3.core.type.EthBool
+import dev.klepto.kweb3.core.type.EthBytes
+
+/**
+ * Implementation of [Multicall3](https://github.com/mds1/multicall) smart
+ * contract.
+ *
+ * @author <a href="http://github.com/klepto">Augustinas R.</a>
+ */
+interface Multicall3 : Web3Contract {
+
+    /**
+     * Aggregates all calls into a single request and returns an array
+     * containing [results][Result] of each call.
+     *
+     * @param calls an ethereum array containing calls for aggregation
+     * @return an ethereum array containing result of each call
+     */
+    @View
+    suspend fun aggregate3(calls: EthArray<Call>): EthArray<Result>
+
+    /**
+     * Contains a single smart contract call request. If `allowFailure` is set
+     * to `false`, failure of this call will cause entire aggregation request
+     * to fail.
+     *
+     * @param target the target contract address
+     * @param allowFailure marks that aggregation request is allowed to
+     *     continue upon failure of this call
+     * @param callData the call-data contained in ethereum bytes
+     */
+    data class Call(val target: EthAddress, val allowFailure: EthBool, val callData: EthBytes) : EthStructContainer
+
+    /**
+     * Contains result of a single smart contract call.
+     *
+     * @param success true if call was successful
+     * @param returnData ethereum bytes containing return data
+     */
+    data class Result(val success: EthBool, val returnData: EthBytes) : EthStructContainer
+
+}
