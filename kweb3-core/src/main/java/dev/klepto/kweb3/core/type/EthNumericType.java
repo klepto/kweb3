ed2ece99abbde6f23f03f12b4272e2eaa10befdd
klepto <klepto@inbox.lt>
2024-02-19 11:03:08 +0200

    feat: Implement EthNumericType converting methods

--- kweb3-core/src/main/java/dev/klepto/kweb3/core/type/EthNumericType.java
+++ kweb3-core/src/main/java/dev/klepto/kweb3/core/type/EthNumericType.java
@@ -3,6 +3,7 @@ package dev.klepto.kweb3.core.type;
 import dev.klepto.kweb3.core.util.hash.Keccak;
 import org.jetbrains.annotations.NotNull;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import static dev.klepto.kweb3.core.util.hash.Keccak256.keccak256Checksum;
@@ -21,6 +22,62 @@ public interface EthNumericType extends EthType {
      */
     @NotNull
     BigInteger value();
+
+    /**
+     * Returns numeric value as <code>integer</code> value.
+     *
+     * @return the numeric value as integer
+     */
+    default int toInt() {
+        return value().intValue();
+    }
+
+    /**
+     * Returns numeric value as <code>long</code> value.
+     *
+     * @return the numeric value as long
+     */
+    default long toLong() {
+        return value().longValue();
+    }
+
+    /**
+     * Returns numeric value as <code>double</code> value with <code>18</code> decimals.
+     *
+     * @return the numeric value as double with 18 decimal places
+     */
+    default double toDouble() {
+        return toDouble(18);
+    }
+
+    /**
+     * Returns numeric value as <code>double</code> value.
+     *
+     * @param decimals the amount of decimal places
+     * @return the numeric value as double
+     */
+    default double toDouble(int decimals) {
+        return toDecimal(decimals).doubleValue();
+    }
+
+    /**
+     * Returns numeric value as {@link BigDecimal}> value with <code>18</code> decimals.
+     *
+     * @return the numeric value as big decimal with 18 decimal places
+     */
+    default BigDecimal toDecimal() {
+        return toDecimal(18);
+    }
+
+    /**
+     * Returns numeric value as {@link BigDecimal} value.
+     *
+     * @param decimals the amount of decimal places
+     * @return the numeric value as big decimal
+     */
+    default BigDecimal toDecimal(int decimals) {
+        return new BigDecimal(value(), decimals);
+    }
 
     /**
      * Converts numeric value to hexadecimal string.
