f46d199aa338483e95a83cb73364c0ab16bede13
klepto <klepto@inbox.lt>
2024-02-21 19:48:50 +0200

    feat: Multicall builder callRange implementation

--- kweb3-kotlin/src/main/kotlin/dev/klepto/kweb3/kotlin/multicall/MulticallBuilder.kt
+++ kweb3-kotlin/src/main/kotlin/dev/klepto/kweb3/kotlin/multicall/MulticallBuilder.kt
@@ -8,17 +8,33 @@ import dev.klepto.kweb3.kotlin.contracts.Multicall3
  * @author <a href="http://github.com/klepto">Augustinas R.</a>
  */
 class MulticallBuilder<T>(
-    private val contract: Multicall3,
-    private val calls: MutableList<suspend () -> T> = mutableListOf()
+        private val contract: Multicall3,
+        private val calls: MutableList<suspend () -> T> = mutableListOf()
 ) {
 
     /**
      * Appends a new call to the list of calls associated with builder.
      *
+     * @param call the call code
      * @return this builder instance
      */
     fun call(call: suspend () -> T): MulticallBuilder<T> {
         calls.add(call)
+        return this
+    }
+
+    /**
+     * Appends a new calls to the list by supplying range indices to a call
+     * function.
+     *
+     * @param range the range of indices
+     * @param call the call code
+     * @return this builder instance
+     */
+    fun callRange(range: IntRange, call: suspend (index: Int) -> T): MulticallBuilder<T> {
+        for (i in range) {
+            calls.add(suspend { call(i) })
+        }
         return this
     }
 
