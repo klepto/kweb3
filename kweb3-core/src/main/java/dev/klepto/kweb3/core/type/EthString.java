3bd7e15c49fd13173c27ebbc0bf016b3d2215c2d
klepto <klepto@inbox.lt>
2024-03-05 23:49:59 +0200

    chore: EthString clean-up

--- kweb3-core/src/main/java/dev/klepto/kweb3/core/type/EthString.java
+++ kweb3-core/src/main/java/dev/klepto/kweb3/core/type/EthString.java
@@ -1,34 +1,78 @@
 package dev.klepto.kweb3.core.type;
 
+import lombok.With;
+import lombok.val;
 import org.jetbrains.annotations.NotNull;
 
+import java.math.BigDecimal;
+
 /**
- * Represents ethereum <code>string</code> data type.
+ * Container for <code>ethereum string</code> value.
  *
  * @param value the string value
  * @author <a href="http://github.com/klepto">Augustinas R.</a>
  */
+@With
 public record EthString(String value) implements EthType {
 
     /**
-     * Empty string constant.
+     * Empty <code>ethereum string</code> constant.
      */
     public static final EthString EMPTY = string("");
 
+    /**
+     * Returns string representation of this <code>ethereum string</code>.
+     *
+     * @return the string representation of this <code>ethereum string</code>.
+     */
     @Override
     public String toString() {
         return "string(" + value + ")";
     }
 
+    /**
+     * Returns hash code of this <code>ethereum string</code>.
+     *
+     * @return hash code of this <code>ethereum string</code>
+     */
     @Override
     public int hashCode() {
         return value.hashCode();
+    }
+
+    /**
+     * Compares this <code>ethereum string</code> to the specified object.
+     *
+     * @param object the object to compare with
+     * @return true if the objects are the same; false otherwise
+     */
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object == this) {
+            return true;
+        }
+        if (object instanceof EthString other) {
+            return value.equals(other.value);
+        }
+        return false;
     }
 
     /* Solidity style string initializers */
     @NotNull
     public static EthString string(@NotNull String value) {
         return new EthString(value);
+    }
+
+    @NotNull
+    public static EthString string(@NotNull Number value) {
+        if (value instanceof Float || value instanceof Double || value instanceof BigDecimal) {
+            return new EthString(value.toString());
+        }
+
+        val intValue = EthNumericType.parseBigInteger(value);
+        return new EthString(intValue.toString());
     }
 
 }
