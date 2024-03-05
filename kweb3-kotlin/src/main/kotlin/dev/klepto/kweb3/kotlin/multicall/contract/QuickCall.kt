357f9ce499518c0f5ec4893f8e16e611f9e79787
klepto <klepto@inbox.lt>
2024-03-06 00:10:28 +0200

    fix: Compile errors due to new auto-generated kotlin extensions

--- kweb3-kotlin/src/main/kotlin/dev/klepto/kweb3/kotlin/multicall/contract/QuickCall.kt
+++ kweb3-kotlin/src/main/kotlin/dev/klepto/kweb3/kotlin/multicall/contract/QuickCall.kt
@@ -20,7 +20,7 @@ interface QuickCall : MulticallExecutor {
 
     override suspend fun execute(allowFailure: Boolean, calls: List<MulticallExecutor.Call>): List<EthBytes?> {
         // TODO: Implement proper endpoint resolution.
-        val gasLimit = uint256(client.network.endpoints()[0].gasCap.longValue() / calls.size)
+        val gasLimit = uint256(client.network.endpoints()[0].gasCap.toLong() / calls.size)
         val addrs = array(calls.map { it.address })
         val datas = array(calls.map { it.data })
         val result = execute(gasLimit, gasLimit, addrs, datas)
