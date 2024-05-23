d3d60e1fc7aa0812b6bec5c7780c48cf5b9d3cb6
rmcmk <ryley.kimmel@live.com>
2024-05-23 19:18:32 -0400

    feat: jdk 21 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 group = "dev.klepto.kweb3"
 version = "0.0.1"
 
+plugins {
+    id("io.freefair.lombok") version "8.6" apply false
+}
+
 subprojects {
     apply {
         plugin("java-library")
+        plugin("io.freefair.lombok")
     }
 
     repositories {
--- kweb3-core/build.gradle.kts
+++ kweb3-core/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     api(libs.unreflect)
     api(libs.google.guava)
 
-
     implementation(libs.jetbrains.annotations)
     implementation(libs.google.gson)
     implementation(libs.headlong)
@@ -13,10 +12,6 @@ dependencies {
 
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
-
-    implementation(libs.lombok)
-    annotationProcessor(libs.lombok)
-    testAnnotationProcessor(libs.lombok)
 }
 
 tasks.test {
